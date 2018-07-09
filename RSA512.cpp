6658db7c873748da2b83a1943a22648dae026500
Bharadwaj <32398444+bharathsharma95@users.noreply.github.com>
2018-07-09 13:56:23 -0600

    Update RSA512.cpp

--- RSA512.cpp
+++ RSA512.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-//# define NDEBUG
 #define AP_INT_MAX_W 2048
 #include <ap_int.h>
 
 using namespace std;
 
+ap_uint<512> temp_arr[20] = {0};
+ap_uint<512> a_arr[20] = {0};
+ap_uint<512> b_arr[20] = {0};
+ap_uint<512> x_arr[20] = {0};
+ap_uint<512> y_arr[20] = {0};
+
 int test();
 
-ap_uint<512> modinv(ap_uint<512> a, ap_uint<512> m);
-ap_uint<512> gcdExtended(ap_uint<512> a, ap_uint<512> b, ap_uint<512> *x, ap_uint<512> *y);
+ap_uint<512> modinv(ap_uint<512> a, ap_uint<512> b);
 ap_uint<512> modexp(ap_uint<512> base, ap_uint<512> exp, ap_uint<512> n_modulus);
 
 int test()
@@ -16,7 +20,7 @@ int test()
 	ap_uint<256> p = "4669523849932130508876392554713407521319117239637943224980015676156491";
 	ap_uint<256> q = "4906275427767802358357703730938087362176142642699093827933107888253709";
 
-	ap_uint<512> n = p * q;							//n
+	ap_uint<1024> n = p * q;							//n
 	cout << "n is: " << n << "\n";
 
 	ap_uint<256> p_1 = p-1;
@@ -29,11 +33,11 @@ int test()
 	cout << "e is: " << e << "\n";
 
 	ap_uint<512> m = 65;								//m
-	cout << "m is: " << m << "\n\n\n";
+	cout << "m is: " << m << "\n";
 
 	cout << "\n****************************************************************************\n";
 	ap_uint<512> d = modinv(e,phi_n);					//d
-	cout << "d is: " << d << "\n\n\n";
+	cout << "d is: " << d << "\n";
 	cout << "****************************************************************************\n";
 
 	ap_uint<512> C = modexp(m,e,n);					//Cipher  = m^e mod n
@@ -48,57 +52,145 @@ int test()
 }
 
 
-
-ap_uint<512> modinv(ap_uint<512> a, ap_uint<512> phi_n)
+ap_uint<512> modinv(ap_uint<512>a, ap_uint<512>b)
 {
-		ap_uint<512> x, y;
-		ap_uint<512> g = gcdExtended(a, phi_n, &x, &y);
-		cout <<"gcd is: "<<g<<"\n";
+
+	ap_uint<16> count = 0;
+	ap_uint<512> temp = 0;
+	//cout<< "\n******************************************\n\n";
+
+	a_arr[count] = a;
+	b_arr[count] = b;
+	ap_uint<512> phi_n = b;
+	//ap_uint<512> b_0 = b;
+	//cout << "a_arr[" << count << "] : " << a_arr[count] << "\n";
+	//cout << "b_arr[" << count << "] : " << b_arr[count] << "\n\n";
+	//count++;
+
+
+	while(a!=0)
+	{
+		temp = a;
+		temp_arr[count] = temp;
+
+		a = b%a;
+		a_arr[count+1] = a;
+
+		b = temp;
+		b_arr[count+1] = b;
+
+		//cout << "a_arr[" << count << "] : " << a_arr[count] << "\n";
+		//cout << "b_arr[" << count << "] : " << b_arr[count] << "\n\n";
+		count++;
+	}
+
+
+	//count is 15 until now
+
+	// when a == 0; setting values of pointers '*x = 0' and '*y = 1'
+
+	ap_uint<512> gcd = b;
+	x_arr[count] = 0; y_arr[count] = 1;
+
+	cout << "x_arr[" << count << "] : " << x_arr[count] << "\n";
+	cout << "y_arr[" << count << "] : " << y_arr[count] << "\n";
+	cout << "a_arr[" << count << "] : " << a_arr[count] << "\n";
+	cout << "b_arr[" << count << "] : " << b_arr[count] << "\n";
+	cout << "returning b_arr[" << count << "] as gcd : " << gcd << "\n";
+
+	count--;
+	//cout << "after count-- , count is : " << count << "\n\n";
+
+
+	// updating the values of pointers x & y from recursive calls (from while loop)
+
+		while(count!=0)
+		{
+
+			ap_uint<512> b_arr_temp = b_arr[count];
+			ap_uint<512> a_arr_temp = a_arr[count];
+
+			ap_uint<512> X_arr_count_plus1;
+			ap_uint<512> Y_arr_count_plus1;
+			X_arr_count_plus1 = x_arr[count+1];
+			Y_arr_count_plus1 = y_arr[count+1];
+
+				//calculating *x= y1 - ((b/a) * x1);
+				ap_uint<512> X_arr_count_temp;
+				ap_uint<512> helper1 = (b_arr_temp/a_arr_temp);
+				ap_uint<512> helper2 = (helper1 * X_arr_count_plus1);
+				X_arr_count_temp = Y_arr_count_plus1 - helper2;
+
+					x_arr[count] = X_arr_count_temp;
+					cout << "x_arr[" << count << "] : " << x_arr[count] << "\n";
+
+					//calculating *y=x1
+				ap_uint<512> Y_arr_count_temp;
+				Y_arr_count_temp = x_arr[count+1];
+
+					y_arr[count] = Y_arr_count_temp;
+					cout << "y_arr[" << count << "] : " << y_arr[count] << "\n";
+					cout << "a_arr[" << count << "] : " << a_arr[count] << "\n";
+					cout << "b_arr[" << count << "] : " << b_arr[count] << "\n\n";
+
+			count--;
+		}
+
+
+
+	// When count == 0
+
+			ap_uint<512> b_arr_temp = b_arr[count];
+			ap_uint<512> a_arr_temp = a_arr[count];
+
+			ap_uint<512> X_arr_count_plus1;
+			ap_uint<512> Y_arr_count_plus1;
+
+			X_arr_count_plus1 = x_arr[count+1];
+			Y_arr_count_plus1 = y_arr[count+1];
+
+
+				ap_uint<512> X_arr_count_temp;
+				ap_uint<512> helper1 = (b_arr_temp/a_arr_temp);
+				ap_uint<512> helper2 = (helper1 * X_arr_count_plus1);
+				X_arr_count_temp = Y_arr_count_plus1 - helper2;
+
+					x_arr[count] = X_arr_count_temp;
+					cout << "x_arr[" << count << "] : " << x_arr[count] << "\n";
+
+				ap_uint<512> Y_arr_count_temp;
+				Y_arr_count_temp = x_arr[count+1];
+
+					y_arr[count] = Y_arr_count_temp;
+					cout << "y_arr[" << count << "] : " << y_arr[count] << "\n";
+					cout << "a_arr[" << count << "] : " << a_arr[count] << "\n";
+					cout << "b_arr[" << count << "] : " << b_arr[count] << "\n\n";
+
+
+	// when count == 0 --> end
+
+
+
 
 		ap_uint<512> res;
-			if (g != 1)
-			        cout << "Inverse doesn't exist\n";
-		    else
-		    {
-		        // phi_n is added to handle negative x
-		        res = ((x % phi_n) + phi_n) % phi_n;
-		        cout << "x is : "<< x << "\n";
-		        cout << "Modular multiplicative inverse is " << res <<"\n";
-		    }
+		if (gcd != 1)
+		        cout << "Inverse doesn't exist\n\n\n";
+	    else
+	    {
+	        // phi_n is added to handle negative x
+	    	ap_uint<512> x_arr_temp = x_arr[count];
+	    	//cout << "x_arr_temp : " << x_arr_temp << "\n";
+	    	//cout << "phi_n : " << phi_n << "\n";
+	    	//ap_uint<1024> helper1 = x_arr_temp % phi_n;
+	    	//ap_uint<1024> helper2 = ((x_arr_temp % phi_n) + phi_n);
+
+	        res = ((x_arr_temp % phi_n) + phi_n) % phi_n;
+	        //cout << "Modular multiplicative inverse is " << res <<"\n\n\n";
+	    }
+
 
 		return res;
 }
-
-ap_uint<512> gcdExtended(ap_uint<512> a, ap_uint<512> b, ap_uint<512> *x, ap_uint<512> *y)
-{
-    // Base Case
-    if (a == 0)
-    {
-        *x = 0, *y = 1;
-        cout<<"returning b as gcd="<<b<<"\n\n";
-        cout<<"*x:"<<*x<<"\n";
-        cout<<"*y:"<<*y<<"\n";
-        cout<<"a:"<<a<<"\n";
-        cout<<"b:"<<b<<"\n\n";
-        return b;
-    }
-
-    ap_uint<512> x1, y1; // To store results of recursive call
-    ap_int<512> gcd = gcdExtended(b%a, a, &x1, &y1);
-
-    // Update x and y using results of recursive call
-    *x = y1 - ((b/a) * x1);
-    //cout<<"*x:"<<*x<<"\n";
-    *y = x1;
-    //cout<<"*y:"<<*y<<"\n";
-    cout<<"*x:"<<*x<<"\n";
-    cout<<"*y:"<<*y<<"\n";
-    cout<<"a:"<<a<<"\n";
-    cout<<"b:"<<b<<"\n\n";
-
-    return gcd;
-}
-
 
 
 ap_uint<512> modexp(ap_uint<512> base, ap_uint<512> exp, ap_uint<512> n_modulus)
